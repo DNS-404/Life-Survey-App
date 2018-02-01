2a91293d344466d01f0819294830a8af1138b2b8
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-02-02 00:53:43 +0530

    Add questions

--- src/Survey.js
+++ src/Survey.js
@@ -23,8 +23,19 @@ class Survey extends Component {
   questionSubmit(){
     // TODO:
   }
-  answerSelected(){
-    // TODO:
+  answerSelected(event){
+    var answers = this.state.answers;
+    if(event.target.name === 'answer1'){
+      answers.answer1 = event.target.value;
+    } else if(event.target.name === 'answer2'){
+      answers.answer2 = event.target.value;
+    } else if(event.target.name === 'answer3'){
+      answers.answer3 = event.target.value;
+    }
+
+    this.setState({answers: answers}, function(){
+      console.log(this.state);
+    });
   }
 
   constructor(props){
@@ -32,13 +43,11 @@ class Survey extends Component {
 
     this.state = {
       uid: uuid.v1(),
-      studentName: '',
+      studentName: 'puppu',
       answers: {
         answer1: '',
         answer2: '',
-        answer3: '',
-        answer4: '',
-        answer5: '',
+        answer3: ''
       },
       isSubmitted: false
     };
@@ -62,13 +71,35 @@ class Survey extends Component {
       foot = '';
     } else if (this.state.studentName !== '' && this.state.isSubmitted === false){
       head = <div>
-        <h2>Here are the questions. Please take your time while answering them.</h2>
+        <h2>Hello {this.state.studentName}. <br /> Here are the questions. Please take your time while answering them.</h2>
         <form onSubmit={this.questionSubmit}>
+          <div className="card">
+            <label>Do you worry about what others think?</label> <br/>
+            <input type="radio" name="answer1" value="yes" onChange={this.answerSelected} />Yes, way too much. <br/>
+            <input type="radio" name="answer2" value="no" onChange={this.answerSelected} />No, Not at all. <br/>
+            <input type="radio" name="answer3" value="maybe" onChange={this.answerSelected} />Sometimes. Can't help it. <br/>
+          </div>
+          <div className="card">
+            <label>Are you comfortable in your own skin?</label> <br/>
+            <input type="radio" name="answer1" value="yes" onChange={this.answerSelected} />Yes, of course. <br/>
+            <input type="radio" name="answer2" value="no" onChange={this.answerSelected} />No. No more comments. <br/>
+            <input type="radio" name="answer3" value="confused" onChange={this.answerSelected} />I don't want to think about this. <br/>
+          </div>
+          <div className="card">
+            <label>Are you truly happy?</label> <br/>
+            <input type="radio" name="answer1" value="yes" onChange={this.answerSelected} />Yes.. I guess. <br/>
+            <input type="radio" name="answer2" value="no" onChange={this.answerSelected} />No, Not at all. <br/>
+            <input type="radio" name="answer3" value="confused" onChange={this.answerSelected} />Never really thought about it. <br/>
+          </div>
+          <div className="card">
+            <label>What do you want people to say at your funeral?</label> <br/>
+            <input type="radio" name="answer1" value="positive" onChange={this.answerSelected} />He was a nice man. He'll be missed. <br/>
+            <input type="radio" name="answer2" value="negative" onChange={this.answerSelected} />This garbage deserved to die! <br/>
+            <input type="radio" name="answer3" value="not-remembered" onChange={this.answerSelected} />Who is he again? <br/>
+          </div>
+          <input className="feedback-button" type="submit" value="Submit" />
         </form>
       </div>;
-    } else if (this.state.studentName !== '' && this.state.isSubmitted === true){
-      head = <h1>Thanks, {this.state.studentName} for your time.</h1>
-
     }
 
     return(
